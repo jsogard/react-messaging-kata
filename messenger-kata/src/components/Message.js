005a49cf17baff2e0f8cd22cc3053c1274846f6b
Joe Sogard <jmsogard@gmail.com>
2020-02-16 11:28:51 -0600

    implement full basic template

--- messenger-kata/src/components/Message.js
+++ messenger-kata/src/components/Message.js
@@ -2,23 +2,34 @@ import React from 'react';
 
 export default class Message extends React.Component {
 
-	messageTemplate = "{greeting} {firstName}, and welcome to {company}!";
+	messageTemplate = "{greeting} {firstName}, and welcome to {company}! Room {roomNumber} is now ready for you. Enjoy your stay and let us know if you need anything.";
 	defaultText = '_____';
 
-	getGreeting = () => 'hello';
-	getFirstName = () => { return this.props.guest?.firstName ?? this.defaultText; }
-	getCompany = () => this.props.company?.company ?? this.defaultText;
+	getGreeting = () => {
+		const hours = new Date().getHours();
+		if(hours < 12) {
+			return 'Good morning';
+		} else if(hours < 18) {
+			return 'Good afternoon';
+		} else {
+			return 'Good evening';
+		}
+	}
+	getFirstName = () => this.props.guest?.firstName;
+	getCompany = () => this.props.company?.company;
+	getRoomNumber = () => this.props.guest?.reservation.roomNumber;
 
 	substitutions = {
 		greeting: this.getGreeting,
 		firstName: this.getFirstName,
-		company: this.getCompany
+		company: this.getCompany,
+		roomNumber: this.getRoomNumber
 	};
 
 	constructMessage() {
 		let message = String(this.messageTemplate);
 		for(const sub in this.substitutions) {
-			message = message.replace(`{${sub}}`, this.substitutions[sub]());
+			message = message.replace(`{${sub}}`, this.substitutions[sub]() ?? this.defaultText);
 		}
 		return message;
 	}
